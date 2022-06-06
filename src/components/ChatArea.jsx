2a386532e7230a0b4b120039eb17f2e60c4308cf
Thomas <t.green01q@gmail.com>
2022-06-06 19:08:59 +0100

    added text box

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const ChatArea = () => {
   return (
     <div className="fixed top-[65px] left-[280px] h-full w-screen]">
-      <div className="absolute bottom-[80px] h-[50px] w-[1030px] bg-[#eee]"></div>
+      <div className="absolute bottom-[80px] h-[50px] w-[1030px]  flex items-center">
+        <textarea
+          rows="1" 
+          wrap='soft'
+          name="chatbox"
+          placeholder="Message in #general"
+          className="resize-none placeholder:text-sm placeholder:text-[#7c7c7c] overflow-hidden outline-none h-[30px] pb-[20px] pt-[5px]  pl-11 pr-11 w-[1050px] mx-3 rounded bg-[#484b52] text-[#eee] text-sm"
+        />
+      </div>
     </div>
   );
 };
